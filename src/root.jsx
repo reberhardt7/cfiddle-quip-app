e1cecc4fb31fe7333e72337a84c4c2a2851fd852
Ryan Eberhardt <reberhardt7@gmail.com>
2019-01-29 18:14:24 -0800

    Fix saving URLs in Quip

--- src/root.jsx
+++ src/root.jsx
@@ -2,9 +2,22 @@ import quip from "quip";
 
 const FRAME_HEIGHT = 400;
 
+class CfiddleRoot extends quip.apps.RootRecord {
+    static getProperties() {
+        return {
+            src: "string",
+        };
+    }
+}
+quip.apps.registerClass(CfiddleRoot, "root");
+
 quip.apps.initialize({
     initializationCallback: function(rootNode, params) {
-        const creationUrl = params.creationUrl || '';
+        const rootRecord = quip.apps.getRootRecord();
+        if (params.isCreation) {
+            rootRecord.set('src', params.creationUrl || '');
+        }
+        const creationUrl = rootRecord.get('src');
         const embedUrl = creationUrl.search(/[\&\?]p=/) > -1
                 ? creationUrl.replace('cfiddle.com/', 'cfiddle.com/embed')
                 : 'https://cfiddle.com/embed';
